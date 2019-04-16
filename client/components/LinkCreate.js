dea49fa86f7f45a57d6d9d370ae71dd7eeb1a2db
zdahmed93 <ahmedzribiahmed@gmail.com>
2019-04-16 23:06:20 +0100

    handle invalid url input

--- client/components/LinkCreate.js
+++ client/components/LinkCreate.js
@@ -2,9 +2,22 @@ import React, {Component} from 'react';
 import {Meteor} from 'meteor/meteor';
 
 export default class LinkCreate extends Component {
+    constructor() {
+        super();
+        this.state = {
+            error: ''
+        }
+    }
     handleSubmit = (event) => {
         event.preventDefault();
-        Meteor.call('links.insert', this.refs.url.value);
+        Meteor.call('links.insert', this.refs.url.value, (error) => {
+            if (error) {
+                this.setState({error: 'Please Enter a valid URL !'})
+            } else {
+                this.setState({error: ''});
+                this.refs.url.value = '';
+            }
+        });
         //console.log(this.refs.url.value);
     }
     render() {
@@ -14,6 +27,7 @@ export default class LinkCreate extends Component {
                         <label>Link to shorten</label>
                         <input type="text" ref='url' className="form-control"/>
                 </div>
+                <div className="text-danger">{this.state.error}</div>
                 <button type="submit" className="btn btn-info">Shorten !</button>
             </form>
         )
--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,11 +1,14 @@
 import {Meteor} from 'meteor/meteor'
 import {Mongo} from 'meteor/mongo';
+import validUrl from 'valid-url';
+import {check, Match} from 'meteor/check';
 
 export const Links = new Mongo.Collection('links');
 
 Meteor.methods({
     'links.insert': function(url) {
-        console.log('attempting to save this :', url)
+        //console.log('attempting to save this :', url)
+        check(url, Match.Where(url => validUrl.isUri(url)) );
     }
 });
 
